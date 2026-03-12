8a37f7fbb15d78b53a4bb8aabd1c69e567174bea
Patrick0402 <bruno.patrick0402@gmail.com>
2026-03-11 22:05:52 -0300

    chore: postgres and flyway config

---
+++ autohub/src/main/java/com/autohub/autohub/AutohubApplication.java
@@ -0,0 +1,13 @@
+package com.autohub.autohub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutohubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutohubApplication.class, args);
+	}
+
+}
